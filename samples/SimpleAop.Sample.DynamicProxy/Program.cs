15dc87f9aa72c93ed1fccbe484af483d6fe9db31
Junil Um <umc@mz.co.kr>
2018-07-18 17:30:46 +0900

    빌드 버전 변경 및 주석 제거

--- samples/SimpleAop.Sample.DynamicProxy/Program.cs
+++ samples/SimpleAop.Sample.DynamicProxy/Program.cs
@@ -10,32 +10,13 @@ using SimpleAop.Proxies.Extensions;
 
 namespace SimpleAop.Sample.DynamicProxy
 {
-    public interface IPrint
-    {
-        void PrintMessage(string message);
-    }
-
-    [LoggingAspect]
-    public class Print : IPrint
-    {
-        public void PrintMessage(string message)
-        {
-            Console.WriteLine(message);
-        }
-    }
-    
     class Program
     {
         static void Main(string[] args)
         {
-            var type = DynamicProxyFactory.Create<IPrint, Print>();
-            var obj = (IPrint) Activator.CreateInstance(type);
-            
-            obj.PrintMessage("Hello World");
-
-//            var type = DynamicProxyFactory.Create(typeof(ITestClass), typeof(TestClass));
-//            var obj = (ITestClass) Activator.CreateInstance(type, "A", "B");
-//            obj.Test("A", "B", 3);
+            var type = DynamicProxyFactory.Create(typeof(ITestClass), typeof(TestClass));
+            var obj = (ITestClass) Activator.CreateInstance(type, "A", "B");
+            obj.Test("A", "B", 3);
         }
     }
 }
